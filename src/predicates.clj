15f4b7abcd8eee60ffce64b5e8711269c74329de
Anton <anton.moroz@helsinki.fi>
2017-04-25 21:36:16 +0300

    Jumissa my-some

--- src/predicates.clj
+++ src/predicates.clj
@@ -71,55 +71,32 @@
 (blank? "  \t a")
 (blank? "")
 
-
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler",
-              :birth-year 1947,
-              :death-year 2006})
-(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
-(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
-(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
-
-(def authors #{china, octavia, kay, dick, zelazny})
-
-(def cities {:title "The City and the City" :authors #{china}
-             :awards #{:locus, :world-fantasy, :hugo}})
-(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
-                    :awards #{:hugo}})
-(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
-(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
-(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
-
-(def books #{cities, wild-seed, lord-of-light,
-             deus-irae, ysabel, scanner-darkly})
-
 (defn
   has-award?
   [book award]
   (contains? (:awards book) award)
   )
 
-(has-award? ysabel :world-fantasy)
-(has-award? scanner-darkly :hugo)
-
 (defn HAS-ALL-THE-AWARDS?
   [book awards]
   (every? (fn [x] (has-award? book x)) awards)
   )
 
-(HAS-ALL-THE-AWARDS? cities #{:locus})
-(HAS-ALL-THE-AWARDS? cities #{:locus :world-fantasy :hugo})
-(HAS-ALL-THE-AWARDS? cities #{:locus :world-fantasy :hugo :pulitzer})
-(HAS-ALL-THE-AWARDS? lord-of-light #{:locus :world-fantasy :hugo})
-(HAS-ALL-THE-AWARDS? lord-of-light #{:hugo})
-(HAS-ALL-THE-AWARDS? scanner-darkly #{})
 
+;intuitio - tarkistetaan filtteröidyn joukon koko
+;ongelma - first tyyliset metodit
+;1. korjaus (or) - ei onnistu (molemmin päin)
+;2. korjaus - vaihdetaan filter mappiin
 (defn
   my-some
   [pred a-seq]
-
+  (map (fn [x] (pred x)) a-seq)
   )
+
+(my-some even? [1 3 5 7])
+(my-some even? [1 3 5 7 8])
+(my-some nil? [1 nil 2])
+(my-some first [[false] [1]])
 
 (defn my-every? [pred a-seq]
   :-)
